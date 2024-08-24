feec032cd6d1cb008aa19b5f805e78f7edd8bbe2
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-08-24 13:34:51 +0400

    first commit

---
+++ MentorLearningProject/src/main/java/org/example/mentorlearningproject/MentorLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.mentorlearningproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorLearningProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorLearningProjectApplication.class, args);
+    }
+
+}
